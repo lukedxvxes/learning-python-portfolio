3bff03d4c517433279f462c743d296cd52653cb2
lukedxvxes <lukedaviesweb+1@gmail.com>
2023-05-14 16:47:58 +0700

    add profile section with layout cols, image title and bio

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
+col1, col2, = st.columns(2)
+
+with col1:
+    st.image("images/profile.png", width=400)
+
+with col2:
+    st.title("Luke Davies")
+    content=""" 
+        Add something here about a bio
+    """
+    st.write(content)
